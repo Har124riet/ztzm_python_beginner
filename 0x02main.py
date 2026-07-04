52410f3b68176670ade5dac948528527adee2cb5
Umeana Adaeze <Har124riet>
2026-07-04 09:58:25 -0700

    any message

--- 0x02main.py
+++ 0x02main.py
@@ -1,4 +1,4 @@
-name = input ("what is your name?")
-age = input("how old are you?")
-hobby = input("what is your favorite hobby?")
-print(f"my name is{name}, I am {age} yrs old,and my favourite hobby is {hobby}!")
+name = input ("what is your name? ")
+age = input("how old are you? ")
+hobby = input("what is your favorite hobby? ")
+print(f"my name is {name}, I am {age} yrs old,and my favourite hobby is {hobby}!")
