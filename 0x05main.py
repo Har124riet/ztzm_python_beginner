26ec26b73c5b8ef701df5af34597563e0ef648b6
Umeana Adaeze <Har124riet>
2026-07-05 08:38:46 -0700

    second commit

--- 0x05main.py
+++ 0x05main.py
@@ -1,4 +1,4 @@
-cities = {"London," "Paris," "Los Angeles," "New York," "Tokyo"}
+cities = {"London", "Paris", "Los Angeles", "New york", "Tokyo"}
 print(cities)
 
 cities.add("New york")
@@ -10,12 +10,13 @@ print(cities)
 cities.discard("London")
 print(cities)
 
-print("/n Cities i have visited:")
+print("\n Cities I have visited:")
 for city in cities:
     print(city)
 
 city_to_check = "canada"
-if city in cities:
-    print(f"\nYes, you have visited {cities}.")
+
+if city_to_check in cities:
+    print(f"\nYes, you have visited {city_to_check}.")
 else:
-    print(f"\nNo, you have not visited {cities}.")
+    print(f"\nNo, you have not visited {city_to_check}.")
