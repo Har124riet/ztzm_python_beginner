e911966c56a60cac493fd1ea302e89e199bfe56c
Umeana Adaeze <Har124riet>
2026-07-05 08:55:18 -0700

    second commit

--- 0x06main.py
+++ 0x06main.py
@@ -1,9 +1,10 @@
 age = 64
-if age > 13:
- print("ticket price is $5")
-elif age  > 18:
- print("ticket price is $7")
-elif age > 65:
+
+if age >= 65:
+ print("you are a senior.")
+elif age  >= 18:
  print("ticket price is $10")
+elif age >= 14:
+ print("ticket price is $7")
 else:
- print ("you are a senior")
+ print ("ticket price is $5")
